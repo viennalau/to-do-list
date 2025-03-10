ca488c5b26fdbeca5405ef5b915aa06af46f8177
vienna <viennalau0@gmail.com>
2025-03-10 12:54:55 -0400

    created website

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ from fasthtml.common import *
 
 def render(tasks):
     return Li(f"{tasks.task}",
-           A("Delete", hx_delete=f"/{tasks.id}", hx_swap="outerHTML", target_id=f'tasks-{tasks.id}'),
+           A("Complete", hx_delete=f"/{tasks.id}", hx_swap="outerHTML", target_id=f'tasks-{tasks.id}'),
            id=f'tasks-{tasks.id}'
         ) 
 
 # Initialize FastHTML app with SQLite
 app,rt,tasks,Task = fast_app(
-    'tasks.db', 
+    'tasks.db',
     live=True, 
     render=render,
     id=int, 
@@ -19,51 +19,38 @@ app,rt,tasks,Task = fast_app(
 )
 
 def reset_task_input():
-    return Input(name="task", id="task", placeholder="Task", hx_swap_oob="true", required=True)
+    return Input(name="task", id="task", placeholder="Task Name", hx_swap_oob="true", required=True)
 
-def empty_alert(text):
-    return Div(
-        alert(text)
-        P(text),
-        Button("OK", hx_get="/close_alert", hx_swap_oob="true"),
-        id="alert",
-        open=True
-    ),
-    )
-
-# Route to display the contact list
+# Route to display the to-do list
 @rt('/')
 def get():
-    # create_contacts()
-    # items = [Li(o) for o in contacts()]
     return Titled("Vienna's To-Do List", Div(
-        Div(id="alert"),
-        Ul(*tasks(), id="task_list"),
+        Ul(*tasks(), id="task_list"), 
         H2("Add a new task:"),
         Form(
             reset_task_input(),
-            Button("Add Task", hx_post="/", target_id="task_list", hx_swap="beforeend")
-        )
-    )
-    )
+            Button("Add Task", hx_post="/", target_id="task_list", hx_swap="beforeend", styLe="background-color: pink; color: white; border-color: pink; box-shadow: none;")
+        ),
+        Div(
+            H4(" "),
+            P(id="alert")),
+        ))
 
-# Route to add a new contact
+# Route to add a new task
 @rt('/')
 def post(task: str):
     task = task.strip()
     if not task:
-        alert("Message cannot be empty!")
+        return P("Task cannot be empty!", id="alert", style="color: red;", hx_swap_oob="true")
     
-    new_task = tasks.insert(task=task) 
-    return new_task, reset_task_input()
+    task = task + " "
+    new_task = tasks.insert(task=task)
+    
+    return new_task, reset_task_input(), P("", id="alert", hx_swap_oob="true")
 
 @rt('/{id}')
 def delete(id: int):
     tasks.delete(id)
-
-@rt('/close_alert')
-def close_alert():
-    return Dialog(id="alert", open=False, hx_swap_oob="true")
 
 # Start the server
 serve()
